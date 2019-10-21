793793dcd61ef7566da12dcdd1ab7a6d9c615486
zroyer <zroyer@utexas.edu>
2019-10-21 17:38:54 -0500

    spinning working

---
+++ index.js
@@ -0,0 +1,26 @@
+const process = require('process');
+const readline = require('readline');
+const std = process.stdout;
+
+class Spinner {
+  spin() {
+    std.write('\x1B[?25l');
+    const spinners = ['-', '\\', '|', '/'];
+    let index = 0;
+
+    setInterval(() => {
+      let line = spinners[index];
+
+      if (!line) {
+        index = 0;
+        line = spinners[index];
+      }
+
+      std.write(line);
+      readline.cursorTo(std, 0);
+      index = index >= spinners.length ? 0 : index + 1;
+    }, 100);
+  }
+}
+
+new Spinner().spin();
